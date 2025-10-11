dbb2d3cad899fe624575dad1f3e427808c794c92
tane0776 <herazoana03@gmail.com>
2025-10-11 13:36:31 -0500

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { inject } from '@vercel/analytics';
+
+inject();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
